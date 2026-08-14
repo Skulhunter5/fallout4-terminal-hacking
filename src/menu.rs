4df6427d7b0289791b28ef12cc6ffe44ebcb8736
Skulhunter5 <linus.huster@gmail.com>
2026-08-14 16:57:52 +0200

    Decorate menu

--- src/menu.rs
+++ src/menu.rs
@@ -3,12 +3,13 @@ use ratatui::{
     crossterm::event::{KeyCode, KeyEvent, MouseButton, MouseEvent, MouseEventKind},
     layout::{Offset, Position, Rect, Size},
     style::Style,
+    text::Text,
     widgets::Widget,
 };
 
 use crate::app::{App, Scene};
 
-const HEAD_HEIGHT: u16 = 1;
+const HEAD_HEIGHT: u16 = 2;
 const SPACING: u16 = 1;
 const HEAD_POS: Position = Position::new(0, 0);
 const OPTIONS_POS: Position = Position::new(HEAD_POS.x, HEAD_POS.y + HEAD_HEIGHT + SPACING);
@@ -23,6 +24,7 @@ pub enum MenuResult {
 pub struct Menu {
     head_line: String,
     result: Option<MenuResult>,
+    text: String,
     selected: usize,
     options: Vec<String>,
     clickable: bool,
@@ -39,8 +41,10 @@ impl Menu {
             .collect();
 
         Self {
-            head_line: "Welcome to ROBCO Industries (TM) Termlink".to_owned(),
+            head_line: "Welcome to ROBCO Industries (TM) Termlink\nFallout 4 Hacking Minigame"
+                .to_owned(),
             result: None,
+            text: "Choose a difficulty:".to_owned(),
             selected: 0,
             options,
             clickable: false,
@@ -90,8 +94,15 @@ impl Scene for Menu {
             MouseEventKind::Moved => {
                 let area = Rect::new(0, 0, Self::WIDTH, Self::HEIGHT);
                 let options_area = area
-                    .resize(Size::new(area.width, area.height - HEAD_HEIGHT))
+                    .resize(Size::new(area.width, area.height - HEAD_HEIGHT - SPACING))
                     .offset(Offset::new(OPTIONS_POS.x as i32, OPTIONS_POS.y as i32));
+                let text_height = self.text.lines().count();
+                let options_area = options_area
+                    .resize(Size::new(
+                        options_area.width,
+                        options_area.height - text_height as u16,
+                    ))
+                    .offset(Offset::new(0, text_height as i32));
 
                 if options_area.contains(Position::new(mouse_event.column, mouse_event.row)) {
                     let row = (mouse_event.row - options_area.y) as usize;
@@ -118,25 +129,26 @@ impl Scene for Menu {
 
 impl Widget for &Menu {
     fn render(self, area: Rect, buf: &mut Buffer) {
-        // TODO: improve headline
-        // - include the line "Initializing..." (maybe that's for holotapes)
-        // - Another example ingame: "Museum of Freedom Operations Terminal"
-        //   - Maybe something like "Fallout 4 Hacking Minigame"
         let head_area = area
             .resize(Size::new(area.width, HEAD_HEIGHT))
             .offset(Offset::new(HEAD_POS.x as i32, HEAD_POS.y as i32));
-        // TODO: Maybe add text line like "Choose a difficulty:" right above the options
         let options_area = area
             .resize(Size::new(area.width, area.height - head_area.height))
             .offset(Offset::new(OPTIONS_POS.x as i32, OPTIONS_POS.y as i32));
 
-        self.head_line.as_str().render(head_area, buf);
+        Text::from(self.head_line.as_str()).render(head_area, buf);
 
-        self.options
-            .iter()
-            .zip(options_area.rows())
+        let text_height = self.text.lines().count();
+        options_area
+            .rows()
+            .zip(self.text.lines())
+            .for_each(|(row_area, text_line)| text_line.render(row_area, buf));
+        options_area
+            .rows()
+            .skip(text_height)
+            .zip(&self.options)
             .enumerate()
-            .for_each(|(i, (option, row_area))| {
+            .for_each(|(i, (row_area, option))| {
                 let line = format!("[{}]", option);
                 if i == self.selected {
                     buf.set_style(row_area, Style::new().reversed());
